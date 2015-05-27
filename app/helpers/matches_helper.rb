9ced3ee498e8e57b1bd8fd4e4c187efade74709d
Roberto Achacoso <bertoa@gmail.com>
2015-05-27 13:25:39 -0700

    fix getting user last login

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -1,2 +1,14 @@
 module MatchesHelper
+
+	def get_last_login(match)
+
+		login_list = Array.new
+		match.users.each do |user|
+			login_list << user.last_login
+		end
+
+		return login_list.sort!.first
+
+	end
+
 end
