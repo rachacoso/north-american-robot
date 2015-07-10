9d907ad4b99c3e29a22f6fba7e1be0d946a91922
Roberto Achacoso <bertoa@gmail.com>
2015-07-09 21:25:25 -0700

    add sharing for conversation contact distributors

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,27 +1,53 @@
 class ConversationsController < ApplicationController
 
 	def share
-		if params[:documents_list] && params[:match_id]
+		if params[:share_list] && params[:match_id]
+			
 			b_or_d = @current_user.brand || @current_user.distributor
-			# this_match = b_or_d.matches.find(params[:match_id])
 			shared_docs_list = {}
-			params[:documents_list].each do |k,v|
-				doc = LibraryDocument.find(v)
-				shared_docs_list[doc.filename] = doc.file.url
+			if @current_user.brand
+				params[:share_list].each do |k,v|
+					doc = LibraryDocument.find(v)
+					shared_docs_list[doc.filename] = doc.file.url
+				end
+			elsif @current_user.distributor
+				params[:share_list].each do |k,v|
+					unless v.blank?
+						shared_docs_list[k] = v
+					end
+				end
 			end
-
 			doc_message_text = ""
-			shared_docs_list.each do |filename,url|
-				doc_message_text += "<a href='#{url}'>#{filename}</a><br>"
+			if @current_user.brand
+				shared_docs_list.each do |filename,url|
+					doc_message_text += "<h4><strong><a href='#{url}'>#{filename}</a></strong></h4>"
+				end
+				info_type = "files"
+				not_nil = true
+			elsif @current_user.distributor
+				not_nil = false
+				shared_docs_list.each do |k,v|
+					not_nil = v.blank? ? false : true
+					doc_message_text += "<h4><strong>#{k.gsub(/_/, " ").capitalize}:</strong></h4><p> #{v}</p>"
+				end
+				info_type = "information"
 			end
-			message_text = "
-			#{b_or_d.company_name} has shared the following files with you:<br>
-			#{doc_message_text}
-			".html_safe
 
-			create_message(params[:match_id], message_text)
+			if not_nil
+				message_text = "
+				#{b_or_d.company_name} has shared the following #{info_type}:<br>
+				#{doc_message_text}
+				".html_safe
+				create_message(params[:match_id], message_text)
+			end
 
 		end
+
+
+		mm = b_or_d.matches
+		m = mm.find(params[:match_id])
+		@messages = m.messages.order_by(:c_at.asc)
+		@stage = m.stage
 
     respond_to do |format|
       format.html
@@ -41,8 +67,8 @@ class ConversationsController < ApplicationController
 			m = mm.find(match_id)
 			m.messages << Message.new(recipient: @current_user.type_inverse?, text: text, stage: m.stage, read: false)
 			m.save!
-			@messages = m.messages.order_by(:c_at.asc)
-			@stage = m.stage
+			# @messages = m.messages.order_by(:c_at.asc)
+			# @stage = m.stage
 
 	end
 
