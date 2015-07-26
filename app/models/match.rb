efb40080bb8a9ddc1c9f1f4da08f2db5ac130549
Roberto Achacoso <bertoa@gmail.com>
2015-07-26 14:33:01 -0700

    changed default partnership_terms_length value to 3 years

--- app/models/match.rb
+++ app/models/match.rb
@@ -32,7 +32,7 @@ class Match
   field :ingredient_or_materials_lists, type: String, default: ""
 
   # text fields
-  field :partnership_terms_length, type: String, default: ""
+  field :partnership_terms_length, type: String, default: "3 years"
   field :payment_terms, type: String, default: ""
   field :grant_territory_exclusivity, type: String, default: ""
   field :requested_minimum_marketing_spend, type: String, default: ""
