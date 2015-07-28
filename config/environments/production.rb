351ce72d6d186c967e7e9ce8f19e17100d1e487e
Roberto Achacoso <bertoa@gmail.com>
2015-07-27 17:21:06 -0700

    add mailgun config to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,15 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   # Mailer default URL
   config.action_mailer.default_url_options = { :host => "landing-international.herokuapp.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'landing-international.herokuapp.com', #eg: 'yourappname.herokuapp.com'
+    :authentication => :plain,
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
