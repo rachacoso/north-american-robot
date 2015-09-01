ce0839204f6e3e001b12bd3db7047914382d74ca
Roberto Achacoso <bertoa@gmail.com>
2015-09-01 11:46:04 -0700

    change wording on password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,31 +2,30 @@ class PasswordResetsController < ApplicationController
 	skip_before_action :require_login
 
   def new
-  	render layout: "front"
+  	# render layout: "front"
   end
 
 	def create
 	  user = User.where(email: params[:email]).first
 	  user.send_password_reset if user
-	  redirect_to login_url, :notice => "EMAIL SENT WITH PASSWORD RESET INSTRUCTIONS"
+	  redirect_to login_url, :notice => "AN EMAIL HAS BEEN SENT WITH PASSWORD RESET INSTRUCTIONS"
 	end  
 
 	def edit
 	  @user = User.where(password_reset_token: params[:id]).first
-	  render layout: "front"
+	  # render layout: "front"
 	end
 
 	def update
 	  @user = User.where(password_reset_token: params[:id]).first
 	  if @user.password_reset_sent_at < 2.hours.ago
-	    redirect_to new_password_reset_path, :alert => "Password &crarr; 
-	      reset has expired."
+	    redirect_to new_password_reset_path, :alert => "PASSWORD RESET HAS EXPIRED"
 	  elsif @user.update(password_update_parameters)
-	    redirect_to login_url, :notice => "Password has been reset."
+	    redirect_to login_url, :notice => "YOUR PASSWORD HAS BEEN RESET"
 	  else
 	    render :edit
 	  end
-	  render layout: "front"
+	  # render layout: "front"
 	end
 
 	private
