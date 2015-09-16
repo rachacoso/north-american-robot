32ad8cafe5cad17f148136536b4e37cbc462cda9
Roberto Achacoso <bertoa@gmail.com>
2015-09-16 13:47:06 -0700

    add helper to get prospect share type (brand or distributor)

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,13 @@
 module HomeHelper
+
+	def get_share_type(id)
+		if Brand.find(id)
+			return 'brand'
+		elsif Distributor.find(id)
+			return 'distributor'
+		else
+			return nil
+		end
+	end
+	
 end
