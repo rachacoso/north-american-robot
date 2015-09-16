5cdcf9827efbc81a2d09148f890e46e642fdd675
Roberto Achacoso <bertoa@gmail.com>
2015-09-16 11:15:04 -0700

    add sharing login and password reset for existing users

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -22,16 +22,26 @@ class PasswordResetsController < ApplicationController
 
 	def edit
 	  @user = User.where(password_reset_token: params[:id]).first
+	  @share_id = params[:pshare]
 	  # render layout: "front"
 	end
 
 	def update
 	  @user = User.where(password_reset_token: params[:id]).first
 	  if @user.password_reset_sent_at < 2.hours.ago
-	    redirect_to new_password_reset_path, :alert => "PASSWORD RESET HAS EXPIRED"
+	  	if params[:pshare]
+	  		redirect_to prospect_share_path(params[:pshare]), :alert => "PASSWORD SET/RESET HAS EXPIRED"
+	  	else
+		    redirect_to new_password_reset_path, :alert => "PASSWORD SET/RESET HAS EXPIRED"
+		  end
 	  elsif @user.update(password_update_parameters)
-	    redirect_to login_url, :notice => "YOUR PASSWORD HAS BEEN RESET"
+	  	if params[:pshare]
+	  		redirect_to prospect_share_path(params[:pshare]), :notice => "YOUR PASSWORD HAS BEEN RESET"
+	  	else
+		    redirect_to login_url, :notice => "YOUR PASSWORD HAS BEEN RESET"
+		  end
 	  else
+	  	@share_id = params[:pshare]
 	    render :edit
 	  end
 	  # render layout: "front"
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -13,36 +13,55 @@ class SessionController < ApplicationController
 	end
 
 	def create
-		user = User.where(email: params[:email]).first
-		if user && user.authenticate(params[:password])
-	    if params[:keep_me_logged_in]
-	      cookies.permanent[:auth_token] = user.auth_token
-	    else
-	      cookies[:auth_token] = user.auth_token  
-	    end
-			if user.administrator
-				redirect_to admin_brands_index_url
-			else
-				if params[:share_id] #sharing
-					if user.brand
-						# currently can only share brands, so only visible by distributors
-						redirect_to dashboard_url
-					elsif user.distributor
-						redirect_to view_match_url(params[:share_id], 'na')
-					end
+		@user = User.where(email: params[:email]).first
+		@share_id = params[:share_id]
+
+		if @share_id #is login from a share link
+
+			if @user && @user.authenticate(params[:password])
+				if params[:keep_me_logged_in]
+					cookies.permanent[:auth_token] = @user.auth_token
 				else
-					redirect_to '/dashboard'
+					cookies[:auth_token] = @user.auth_token  
 				end
-			end
-		else
-			if params[:share_id] #sharing
-				flash[:notice] = "INVALID EMAIL OR PASSWORD"
-				redirect_to prospect_share_url(params[:share_id])
+				if @user.administrator
+					@redirect_url = admin_brands_index_url
+				else
+					if @user.brand
+						# currently can only share brands, so only visible by distributors
+						@redirect_url = dashboard_url
+					elsif @user.distributor
+						@redirect_url = view_match_url(@share_id, 'na')
+					end
+				end
 			else
 				flash[:notice] = "INVALID EMAIL OR PASSWORD"
-				redirect_to login_url
 			end
+
+			respond_to do |format|
+				format.js
+			end
+
+		else #is regular login
+
+			if @user && @user.authenticate(params[:password])
+				if params[:keep_me_logged_in]
+					cookies.permanent[:auth_token] = @user.auth_token
+				else
+					cookies[:auth_token] = @user.auth_token  
+				end
+				if @user.administrator
+					redirect_to admin_brands_index_url and return
+				else
+					redirect_to dashboard_url and return
+				end
+			else
+				flash[:notice] = "INVALID EMAIL OR PASSWORD"
+				redirect_to login_url and return
+			end
+
 		end
+
 	end
 
 	def destroy
