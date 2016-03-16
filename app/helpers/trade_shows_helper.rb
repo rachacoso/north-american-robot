c01e806d1b7ddabcbb4d6a3bb65ca3583aba3561
Roberto Achacoso <bertoa@gmail.com>
2016-03-15 20:28:47 -0700

    change tradeshow display in brand/distributor/retailer profiles

--- app/helpers/trade_shows_helper.rb
+++ app/helpers/trade_shows_helper.rb
@@ -1,2 +1,19 @@
 module TradeShowsHelper
+
+	def get_tradeshow_display_info(show)
+		ts = []
+		unless show.country.blank?
+			ts << "<strong>#{show.country}</strong>"
+		end
+		if show.date
+			ts << "#{show.date.strftime('%b %Y')}"
+		end
+		unless show.years_participated.blank?
+			ts << "Participant for #{show.years_participated} years"
+		end
+		unless show.website.blank?
+			ts << "#{link_to 'website', clean_url(show.website), target: 'blank'}"
+		end
+		return ts
+	end
 end
