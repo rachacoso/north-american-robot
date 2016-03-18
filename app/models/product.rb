260c548f9f20c26a099f978727ba04f865f30442
Roberto Achacoso <bertoa@gmail.com>
2016-03-18 13:16:41 -0700

    add price field to product for use on calcuations

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,8 @@ class Product
 
  	field :name, type: String, default: ""
  	field :description, type: String, default: ""
- 	field :msrp, type: String, default: ""
+ 	field :msrp, type: String, default: "" # old version & now will be used for display purposes only
+ 	field :price, type: Integer # store MSRP price as cents (to be used in calculations for orders)
  	field :key_benefits, type: String, default: ""
  	field :country_of_manufacture, type: String, default: ""
  	field :current, type: Mongoid::Boolean
