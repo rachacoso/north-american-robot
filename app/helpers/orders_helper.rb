932e87832fb3e63bab0ca9b7892063cc1aba280d
Roberto Achacoso <bertoa@gmail.com>
2016-03-20 00:24:01 -0700

    add current/pending order functionality & display logic

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,7 +1,11 @@
 module OrdersHelper
 
 	def current_order(brand)
-		return @current_user.get_parent.orders.where(brand_id: brand.id).first
+		return @current_user.get_parent.orders.where(status: "open", brand_id: brand.id).first
+	end
+
+	def pending_order(brand)
+		return @current_user.get_parent.orders.where(status: "pending", brand_id: brand.id).first
 	end
 
 end
