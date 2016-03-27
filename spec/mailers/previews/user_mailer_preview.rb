f421088cff50574940654e32bb7f15c7940667c4
Roberto Achacoso <bertoa@gmail.com>
2016-03-26 19:24:04 -0700

    fix user mailer preview to add password reset token else didn't work

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -3,12 +3,18 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset(User.first,nil)
+		user = User.first
+		user.generate_token(:password_reset_token)
+		user.save!
+		UserMailer.password_reset(user,nil)
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset_shared
   def password_reset_shared
-    UserMailer.password_reset(User.first,Brand.first.id)
+		user = User.first
+		user.generate_token(:password_reset_token)
+		user.save!
+		UserMailer.password_reset(User.first,Brand.first.id)
   end
 
 end
