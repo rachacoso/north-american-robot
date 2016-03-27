d853fe438bdf14e104a3c2b321024d80b169f908
Roberto Achacoso <bertoa@gmail.com>
2016-03-27 15:43:14 -0700

    put preload back; try intitalizer for mongoid

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,7 @@ workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
-# preload_app!
+preload_app!
 
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
@@ -11,5 +11,6 @@ environment ENV['RACK_ENV'] || 'development'
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
+  # ActiveRecord::Base.establish_connection
+  Mongoid.load!("config/mongoid.yml", :production)
 end
