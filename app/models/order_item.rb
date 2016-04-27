7e2314dafd6ddf6f17d8b8fd77d74859ebee0767
Roberto Achacoso <bertoa@gmail.com>
2016-04-27 07:46:44 -0700

    change returned value from order item methods)

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -25,16 +25,16 @@ class OrderItem # for V2 ordering
   	return Product.find(self.product_id)
   end
   def price_in_dollars # convert from the stored price in cents
-    return '%.2f' % (self.price.to_f / 100)
+    return (self.price.to_f / 100)
   end
   def tiered_price_in_dollars # convert from the stored price in cents (default 50%)
-    return '%.2f' % ((self.price.to_f * (1-(self.order.discount.to_f / 100))) / 100)
+    return ((self.price.to_f * (1-(self.order.discount.to_f / 100))) / 100)
   end
   def tiered_price # convert from the stored price in cents (default 50%)
     return (self.price * (1-(self.order.discount.to_f/100)))
   end
   def total_price # in dollars
-  	return (self.quantity * self.tiered_price) / 100
+    return ((self.quantity * self.tiered_price) / 100)
   end
 
 end
