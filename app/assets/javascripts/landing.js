b368c1c9565da003286cc771d4fbe08fccd4e1ee
Roberto Achacoso <bertoa@gmail.com>
2016-05-02 14:16:59 -0700

    add jquery validate

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -486,6 +486,16 @@ function toggleTopBar(thisOne) {
   })
 }
 
+function formcheck() {
+  var fields = $(".ss-item-required")
+        .find("select, textarea, input").serializeArray();
+
+  $.each(fields, function(i, field) {
+    if (!field.value)
+      alert(field.name + ' is required');
+   });
+  console.log(fields);
+}
 
 jQuery.fn.visible = function() {
     return this.css('visibility', 'visible');
