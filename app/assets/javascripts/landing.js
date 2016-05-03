16ab39477ecddf98e2e880d3d21ac2af6a05a91f
Roberto Achacoso <bertoa@gmail.com>
2016-05-03 15:20:40 -0700

    change behavior of ajax waiting indicator

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -485,11 +485,11 @@ function initialize () {
 $(document)
   .ajaxStart(function () {
     $('.ajax-wait').show();
-    $('.overlay#ajax-wait-overlay').show();
+    // $('.overlay#ajax-wait-overlay').show();
   })
   .ajaxStop(function () {
     $('.ajax-wait').hide();
-    $('.overlay#ajax-wait-overlay').hide();
+    // $('.overlay#ajax-wait-overlay').hide();
   });
 
 function toggleTopBar(thisOne) {
