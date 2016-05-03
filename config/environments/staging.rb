b7e5bbfc006e2a79f1dc56f7313b8caf3e730317
Roberto Achacoso <bertoa@gmail.com>
2016-05-02 20:27:10 -0700

    add mailtrap action mailer config for staging env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -67,13 +67,21 @@ Rails.application.configure do
   # Mailer default URL
   config.action_mailer.default_url_options = { :host => "landingintl-stg.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :port           => ENV['MAILGUN_SMTP_PORT'],
+  #   :address        => ENV['MAILGUN_SMTP_SERVER'],
+  #   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  #   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  #   :domain         => ENV['MAILGUN_DOMAIN'],
+  #   :authentication => :plain,
+  # }
   config.action_mailer.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => ENV['MAILGUN_DOMAIN'],
-    :authentication => :plain,
+    :user_name => ENV['MAILTRAP_USER_NAME'],
+    :password => ENV['MAILTRAP_PASSWORD'],
+    :address => 'mailtrap.io',
+    :domain => 'mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
