25e12895d938dd25cbf1c8df6a51399bb7862fc4
Roberto Achacoso <bertoa@gmail.com>
2016-05-15 13:57:11 -0400

    change webhook to use armor event data ; add account bank info event to webhook

--- app/controllers/armor_payments_controller.rb
+++ app/controllers/armor_payments_controller.rb
@@ -52,87 +52,60 @@ class ArmorPaymentsController < ApplicationController
 
 
 	def webhook
+		if params[:event].present?
+			event_type = params[:event][:type].to_i
+			order_event_types = *(0..30) # order events 
+			account_event_types = [1000,1001] # account events 
 
-		# ARMOR ORDER STATUSES
-		# full list at http://www.armorpayments.com/api/classes/ArmorPayments.Api.Entity.Order.html
-		# STATUS_PAID = 2
-		# # Payment for the Order has been made, and is held in escrow
-		# STATUS_SHIPPED = 3
-		# # Goods to complete the order have been shipped
-		# STATUS_DELIVERED = 4
-		# # Goods to complete the order have been delivered to the buyer
-		# STATUS_RELEASED = 5
-		# # Payment for the Order has been released from escrow
-		# STATUS_PENDING_INCOMPLETE = 6
-		# # Seller's banking details are missing or incomplete
-		# STATUS_PENDING_ERROR = 7
-		# # There was an error transferring payment to the seller. This is most often caused by incorrect banking details.
-		# STATUS_DISPUTE = 8
-		# # A Dispute has been initiated for this Order
-		# STATUS_COMPLETE = 9
-		# The Order is complete, and final payment has been made. This is an internal Armor Payments status, and indicates that # Armor Payments has closed the books on this order. This will occur simetime after your users have finished their last interaction with the order. It is included here because if you look up an old order, you may see this status and need to know what it represents, but is for reference only.
-		# STATUS_CANCELLED = 10
-		# # The Order has been cancelled
-		# STATUS_ARBITRATION = 11
-		# # The Dispute for this Order has been escalated to 3rd party arbitration
-		# STATUS_MILESTONE_PROGRESS = 15
-		# # A milestone order with at least one completed milestone, awaiting next milestone completion.
-		# STATUS_MILESTONE_PENDING = 16
-		# # The milestone order has a completed milestone, awaiting release of milestone payment by buyer.
-		# STATUS_SERVICE_PROGRESS = 17
-		# # A non-milestone service order awaiting completion by service provider.
-		# STATUS_SERVICE_PENDING = 18
-		# # A non-milestone services order awaiting payment release by buyer.
-		# STATUS_CANCELLED_FINAL = 98
-		# # Order has been cancelled, and archived
-		# STATUS_ARCHIVE = 99
-		# # The Order is complete, and has been archived
-		# STATUS_ERROR = 255
-		# # There is an error with the Order
-
-		if params[:order].present?
-
-			if order = Order.find_by(armor_order_id: params[:order][:order_id])
-				case params[:order][:status]
-				when 0 # new
-					logger.info "Armor Webhook Order Status 0 (new order): #{order.id}"
-				when 1 # sent
-					logger.info "Armor Webhook Order Status 1 (order sent): #{order.id}"
-				when 2 # paid
-					order.paid
-				when 3 # shipped
-					order.shipped
-				when 4 # delivered
-					order.delivered
-				when 5 # payment released
-					order.completed
-				else # all other statuses
-					order.error(status: params[:order][:status],message: params[:order][:status_name])
+			if order_event_types.include? event_type # get order if order type event
+				get_order(params[:event][:order_id])
+				if @order.blank?
+					render :nothing => true
+					return
 				end
-				# case params[:event][:type]
-				# when 0 # create
-				# 	logger.info "Armor Webhook Order Status 0 (new order): #{order.id}"
-				# when 2 # sent
-				# 	logger.info "Armor Webhook Order Status 1 (order sent): #{order.id}"
-				# when 2 # paid
-				# 	order.paid
-				# when 3 # shipped
-				# 	order.shipped
-				# when 4 # delivered
-				# 	order.delivered
-				# when 6 # payment released
-				# 	order.completed
-				# else # all other statuses
-				# 	order.error(status: params[:event][:type],message: params[:event][:description])
-				# end
-			else
-				logger.info "Armor Webhook Order Not Found: #{params[:order][:order_id]}"
+			elsif account_event_types.include? event_type # get company if account type event
+				get_company(params[:event][:account_id])
+				if @account.blank?
+					render :nothing => true
+					return
+				end
 			end
 
-			# render :text => "Status: #{params[:order][:status]}, Status Name: #{params[:order][:status_name]}"
+			case event_type
+			when 0
+				logger.info "Armor Webhook Event ID 0 (order create): #{@order.id}"
+			when 1
+				logger.info "Armor Webhook Event ID 1 (order sent): #{@order.id}"
+			when 2
+				@order.paid
+			when 3
+				@order.shipped
+			when 4
+				@order.delivered
+			when 5
+				# order dispute method TBD
+			when 6
+				@order.completed
+			when 1001
+				@company.set_armor_bank
+			else
+				logger.info "Armor Webhook UNHANDLED EVENT -- TYPE: #{event_type} DESCRIPTION: #{params[:event][:description]}"
+			end
 		end
 		render :nothing => true
 	end
 
+	private
+
+	def get_order(order_id)
+		unless @order = Order.find_by(armor_order_id: order_id)
+			logger.info "Armor Webhook Order Not Found. Order ID: #{order_id}"
+		end
+	end
+	def get_company(account_id)
+		unless @company = Brand.find_by(armor_account_id: account_id) || Retailer.find_by(armor_account_id: account_id) || Distributor.find_by(armor_account_id: account_id)
+			logger.info "Armor Webhook Company (w/ account) Not Found. Account ID: #{account_id}"
+		end
+	end
 
 end
