6898a79c3ad8451da81f81160a8fccf947290316
Roberto Achacoso <bertoa@gmail.com>
2016-10-10 07:10:33 +0100

    update ajax for buyer profile > terms

--- app/assets/javascripts/utilities.js
+++ app/assets/javascripts/utilities.js
@@ -1,3 +1,15 @@
+$('.auto-form-text').on('change', function(e){
+    var thisForm = $(this).parents('form');
+    if (typeof timer !== 'undefined') {
+        clearTimeout(timer);
+    }
+    var timer = window.setTimeout( (function( e ) {
+        return function() {
+            $(thisForm).submit();
+        };
+    }( e )), 500);
+});
+
 $('.auto-form').on('change', function(e){
     var thisForm = $(this).parents('form');
     $(thisForm).submit(); 
