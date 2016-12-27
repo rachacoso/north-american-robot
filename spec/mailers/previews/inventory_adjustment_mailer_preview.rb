7beca68d6ee78c1ddfb91d7233018013d8b1e2f5
Roberto Achacoso <bertoa@gmail.com>
2016-12-27 11:16:04 -0500

    add associated requests/shipments notices to mailers

--- spec/mailers/previews/inventory_adjustment_mailer_preview.rb
+++ spec/mailers/previews/inventory_adjustment_mailer_preview.rb
@@ -16,7 +16,12 @@ class InventoryAdjustmentMailerPreview < ActionMailer::Preview
   # Preview this email at http://landing.dev/rails/mailers/inventory_adjustment_mailer/request_updated
   def request_updated
     adjustment = InventoryAdjustment.where(type: "requested").first
-    previous_data = { units: 250, comment: "here are the old comments" }
+    previous_data = { 
+      units: 250, 
+      comment: "here are the old comments", 
+      associated_shipments: adjustment.associated_shipments, 
+      complete: adjustment.complete? 
+    }
     InventoryAdjustmentMailer.send_notice(
       adjustment: adjustment, 
       type: "request_updated", 
@@ -42,7 +47,13 @@ class InventoryAdjustmentMailerPreview < ActionMailer::Preview
   # Preview this email at http://landing.dev/rails/mailers/inventory_adjustment_mailer/shipment_updated
   def shipment_updated
     adjustment = InventoryAdjustment.where(type: "shipment").first
-    previous_data = { units: 250, comment: "here are the old comments", ship_date: DateTime.now - 3.days }
+    previous_data = { 
+      units: 250, 
+      comment: "here are the old comments", 
+      ship_date: DateTime.now - 3.days,
+      associated_requests: adjustment.associated_requests,
+      associated_received_shipments: adjustment.associated_received_shipments
+    }
     InventoryAdjustmentMailer.send_notice(
       adjustment: adjustment, 
       type: "shipment_updated", 
