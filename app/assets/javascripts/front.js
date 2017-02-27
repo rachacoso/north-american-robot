d3603df7da3f18174af48b268d664b06b9039272
Roberto Achacoso <bertoa@gmail.com>
2017-02-27 14:29:39 -0800

    updates to new brand/retailer/distributor signup pages

--- app/assets/javascripts/front.js
+++ app/assets/javascripts/front.js
@@ -10,6 +10,16 @@ $(document).ready(function() {
 		singleItem:true
   });
 
+  $("#new-brand-carousel").owlCarousel({
+		// navigation : true, // Show next and prev buttons
+		loop:true,
+		slideSpeed : 300,
+		autoPlay: 5000,
+		stopOnHover:true,
+		paginationSpeed : 400,
+		singleItem:true
+  });
+
   $("input[type='radio']#user_type_brand, input[type='radio']#user_type_retailer, input[type='radio']#user_type_distributor").on('change', function (e) {
   	if ($(this).val()=="brand")
   	{
